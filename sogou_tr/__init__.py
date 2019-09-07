ac34c29cc774102265212564a27bda5794198eba
ffreemt <yucongo+fmt@gmail.com>
2019-09-07 12:01:34 +0800

    __pycache__ in .gitignore

--- sogou_tr/__init__.py
+++ sogou_tr/__init__.py
@@ -1,7 +1,9 @@
+'''
+version__ = '0.0.2'
+date__ = '2019.7.18'
+'''
 from sogou_tr.sogou_tr import sogou_tr
 
-# __version__ = '0.0.2'
-# __date__ = '2019.7.18'
 __version__ = '0.0.3'
 __date__ = '2019.9.7'
 VERSION = __version__

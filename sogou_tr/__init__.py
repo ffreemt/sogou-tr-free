5cdeccc0543a791c792386f997f377237b19c811
ffreemt <yucongo+fmt@gmail.com>
2019-09-17 12:09:49 +0800

    v0.0.4 removed f-string to accomodate Python 3.5

--- sogou_tr/__init__.py
+++ sogou_tr/__init__.py
@@ -1,9 +1,11 @@
 '''
 version__ = '0.0.2'
 date__ = '2019.7.18'
+__version__ = '0.0.3'
+__date__ = '2019.9.7'
 '''
 from sogou_tr.sogou_tr import sogou_tr
 
-__version__ = '0.0.3'
-__date__ = '2019.9.7'
+__version__ = '0.0.4'
+__date__ = '2019.9.19'
 VERSION = __version__
